5b6b1bd8f4a6f17ec7e0d2c9d4380e8b07fb9a5d
gge0866 <gge0866@louisiana.edu>
2022-01-19 15:54:21 -0600

    Changed system; include step response

--- Matlab/Plotting_poles_zeros.m
+++ Matlab/Plotting_poles_zeros.m
@@ -1,11 +1,10 @@
 clc
 clear
 
-% Plot poles and zeros for a mass-spring-damper system fixed to a wall with
-    % external force input
+% Plot poles and zeros for a mass-spring-damper system with a seismic input
 
 % The transfer function for the system is:
-% X(s)/F(s) = (1/m)/(s^2 + 2*zeta*w_n*s + w_n^2)
+% X(s)/F(s) = (2*zeta*w_n*s + w_n^2)/(s^2 + 2*zeta*w_n*s + w_n^2)
 
 % Define the system parameters that will be used for evaluation
 m = 1; % mass (kg)
@@ -13,11 +12,26 @@ zeta = 0.1; % damping ratio
 w_n = 2*pi; % natural frequency (rad/sec)
 
 % Define the numerator and denominator of the transfer function
-num = [1/m];
+num = [2*zeta*w_n w_n^2];
 den = [1 2*zeta*w_n w_n^2];
 
 % Now define the transfer function by passing `num` and `den` to the tf()
 % function
-G = tf(num,den);
-pzmap(G)
-grid on
+sys = tf(num,den);
+
+% The poles and zeros for this can be plotted using the pzmap() function
+figure(1)
+pzmap(sys)
+grid off
+
+% Now let's plot the time response to a step input (or step response)
+
+time = 0:0.05:5; % time samples (initial_time:time_spacing:final_time
+
+[xout, t] = step(sys,time);
+
+figure(2)
+plot(t,xout)
+title('Step Response')
+xlabel('Time (s)')
+ylabel('Position (m)')
