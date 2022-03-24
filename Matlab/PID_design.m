479a632571f96aab3b13be9184a1a7aaa8767dc8
gge0866 <gge0866@louisiana.edu>
2022-03-23 21:11:02 -0500

    Modified plotting for PID design solutions

--- Matlab/PID_design.m
+++ Matlab/PID_design.m
@@ -1,6 +1,14 @@
 clc
 clear
 
+% % The code used to solve two problems is included in this script.
+% % Comment out the lines for the problem that you are not currently trying
+% % to solve
+%
+% (Hint: You can highlight groups of lines to comment/uncomment
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % % Solution for problem AP7.14
 % % Y/R characteristic equation
 % 
@@ -28,8 +36,17 @@ clear
 % 
 % sys_CL = (10*KU*s)/(s^2*(s+10)*(s+7.5)+10*KU*s);
 % 
+% [CL_output,time]=step(sys_CL,5);
+% 
 % figure(2)
-% step(sys_CL,5)
+% plot(time,CL_output,'red','LineWidth',2)
+% grid on
+% box off
+% ax=gca;
+% ax.FontSize=16;
+% title('Ultimate Gain Response')
+% xlabel('Time (s)','FontSize',22)
+% ylabel('Response','FontSize',22)
 % 
 % % We can now find our other gains using the table
 % 
@@ -39,38 +56,59 @@ clear
 % 
 % sys_CL = (10*(KD*s^2+Kp*s+KI))/(s^2*(s+10)*(s+7.5)+10*(KD*s^2+Kp*s+KI));
 % 
+% [CL_output,time]=step(sys_CL,5);
+% 
 % figure(3)
-% step(sys_CL,5)
+% plot(time,CL_output,'red','LineWidth',2)
+% grid on
+% box off
+% ax=gca;
+% ax.FontSize=16;
+% title('Step Response')
+% xlabel('Time (s)','FontSize',22)
+% ylabel('Response','FontSize',22)
 % 
 % % We also need to plot the disturbance step response
 % % Use TF for Y/Td
-%
+% 
 % sys_dist = 10*s/(s^2*(s+10)*(s+7.5)+10*(KD*s^2+Kp*s+KI));
 % 
+% [dist_output,time]=step(sys_dist,5);
+% 
 % figure(4)
-% step(sys_dist,5)
+% plot(time,dist_output,'red','LineWidth',2)
+% grid on
+% box off
+% ax=gca;
+% ax.FontSize=16;
+% title('Disturbance Response')
+% xlabel('Time (s)','FontSize',22)
+% ylabel('Response','FontSize',22)
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Solution for problem DP7.7
 
-% Set up root locus for changes in Kp and KI to see how changing each
-%   changes the pole locations and response characteristics
+% Set up root locus for changes in Kp and KI to see how changing each gain
+    % changes the pole locations and response characteristics
+    
+% Use the root locus plots to understand how changing gains changes the
+    % step response
 
 % Root locus for Kp
 
 s=tf('s');
 
-KI=1;
+KI=0;
 sys_Kp = s/(s*(s+1)*(0.5*s+1)+KI);
-% sys_Kp = s/(s*(s+1)*(0.5*s+1));
 
 figure(1)
 rlocus(sys_Kp)
 
 % Root locus for KI
 
-Kp=1;
-% sys_KI = 1/(s*(s+1)*(0.5*s+1));
+Kp=0;
 sys_KI = 1/(s*(s+1)*(0.5*s+1)+Kp*s);
 
 figure(2)
@@ -83,6 +121,14 @@ Kv = KI
 % We can check the response characteristics on the step response
 
 sys_CL = (Kp*s + KI)/(s*(s+1)*(0.5*s+1)+Kp*s+KI);
+[output,time]=step(sys_CL,10);
 
 figure(3)
-step(sys_CL,10)
+plot(time,output,'red','LineWidth',2)
+grid on
+box off
+ax=gca;
+ax.FontSize=16;
+title('Position Response')
+xlabel('Time (s)','FontSize',22)
+ylabel('Position','FontSize',22)
